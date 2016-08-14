ccc5705cf922d726e18e863bf21084461bd2080e
brenopolanski <breno.polanski@gmail.com>
2016-08-14 19:38:00 -0300

    Add projects function

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -102,5 +102,116 @@ var work = {
   }
 };
 
+var projects = {
+  projects: [
+    {
+      'title': 'Notifier for Udacity Reviewer',
+      'dates': 'July 2016',
+      'description': 'The extension checks for new notifications every minute and displays notifications that arrived after the last check if there are any. Clicking on the notification opens it on Udacity Dashboard.',
+      'images': ['images/notifier-udacity-1.png', 'images/notifier-udacity-2.png']
+    },
+    {
+      'title': 'CSS Flexbox snippets',
+      'dates': 'Dezember 2015',
+      'description': 'CSS Flexbox for Sublime Text and Atom editor. With more than 3K download.',
+      'images': ['images/css-flexbox-snippets.gif']
+    },
+    {
+      'title': 'A Saiku REST API client for Node.js',
+      'dates': 'June 2015',
+      'description': 'This client is designed to make it easy for your Node.js application to request specific resources from Saiku. It uses a query builder-style syntax to let you craft the request being made to the Saiku endpoints, then returns the API server is response to your application as a JavaScript object.',
+      'images': []
+    },
+    {
+      'title': 'SIGSAB - Sistema de Gestão da Informação e do Conhecimento do Semiárido Brasileiro',
+      'dates': 'September 2014',
+      'description': 'This projects aim is to institutionalize, consolidate and operationalize a computerized information and knowledge management system, through a databank linked to a Geographical Information System (Sistema de Informações Geográficas – SIG) to generate scientific information connected to popular knowledge, aiming at subsidizing the formulation of policies that match the regions context. Another goal is to support other strategic studies and offer relevant services to policy and decision makers.',
+      'images': ['images/sigsab.png']
+    },
+    {
+      'title': 'Saiku UI Plugin Boilerplate',
+      'dates': 'July 2014',
+      'description': 'A jump-start for Saiku UI plugins development.',
+      'images': []
+    },
+    {
+      'title': 'CSS Comments snippets',
+      'dates': 'May 2014',
+      'description': 'CSS comments snippets for Sublime Text. With more than 7K download.',
+      'images': ['images/css-comments-snippets.gif']
+    },
+    {
+      'title': 'Web App Boilerplate',
+      'dates': 'August 2013',
+      'description': 'Project designed to create web applications in WebView to Android.',
+      'images': []
+    },
+    {
+      'title': 'Meu Drink App for Windows 8',
+      'dates': 'May 2013',
+      'description': 'Meu Drink application offers its users the best drink recipes. App available for download in the Windows Store Brazil.',
+      'images': ['images/meu-drink-app-win8.jpeg']
+    },
+    {
+      'title': 'Tic tac toe Paper App for Windows 8',
+      'dates': 'April 2013',
+      'description': 'Classic game tic tac toe for two players (not online). The player who succeeds in placing three respective marks (X and O) in a horizontal, vertical, or diagonal row wins the game.',
+      'images': ['images/tic-tac-toe-paper-app-win8.jpeg']
+    },
+    {
+      'title': 'Descontos na Net App for Windows 8',
+      'dates': 'March 2013',
+      'description': 'The Application Descontos na Net its users the best offers from 10 shops of the largest internet e-commerce in Brazil. App available for download in the Windows Store Brazil.',
+      'images': ['images/descontos-na-net-app-win8.jpeg']
+    },
+    {
+      'title': 'Game Hero Eros - GGJ 2013',
+      'dates': 'January 2013',
+      'description': 'The Global Game Jam (GGJ) is the world is largest game jam event (game creation) taking place around the world at physical locations. It is all condensed into a 48 hour development cycle. The GGJ encourages people with all kinds of backgrounds to participate and contribute to this global spread of game development and creativity.',
+      'images': ['images/game-hero-eros.jpg']
+    },
+    {
+      'title': 'SENAI Student Portal',
+      'dates': 'July 2012',
+      'description': 'Web system developed at SENAI-PB, for students visualize class schedules, notes and fill institutional forms.',
+      'images': []
+    },
+    {
+      'title': 'SGM - Sistema Gerenciador de Metas',
+      'dates': 'May 2011',
+      'description': 'Web system developed at SENAI-PB to work in the strategic management of the institution.',
+      'images': []
+    },
+    {
+      'title': 'Site Descontos na Net',
+      'dates': 'May 2010',
+      'description': 'Web site that gathers coupons of the largest e-commerce stores in Brazil.',
+      'images': []
+    }
+  ],
+  display: function() {
+    if (projects.projects.length > 0) {
+      $('#projects').append(HTMLprojectStart);
+
+      for (var i = 0, iLen = projects.projects.length; i < iLen; i++) {
+        var formattedTitle = HTMLprojectTitle.replace(DATA, projects.projects[i].title);
+        var formattedDates = HTMLprojectDates.replace(DATA, projects.projects[i].dates);
+        var formattedDescription = HTMLprojectDescription.replace(DATA, projects.projects[i].description);
+
+        $('.project-entry').append(formattedTitle);
+        $('.project-entry').append(formattedDates);
+        $('.project-entry').append(formattedDescription);
+
+        for (var j = 0, jLen = projects.projects[i].images.length; j < jLen; j++) {
+          var formattedImages = HTMLprojectImage.replace(DATA, projects.projects[i].images[j]);
+
+          $('.project-entry').append(formattedImages);
+        }
+      }
+    }
+  }
+};
+
 bio.display();
 work.display();
+projects.display();
