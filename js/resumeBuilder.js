85baa8277bac579129c7a65c8d48436d3ddae55b
brenopolanski <breno.polanski@gmail.com>
2016-08-14 18:59:59 -0300

    Add work function

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -43,4 +43,64 @@ var bio = {
   }
 };
 
+var work = {
+  jobs: [
+    {
+      employer: 'Meteorite BI',
+      title: 'Front End Software Engineer',
+      location: 'Brazil',
+      dates: 'September 2014 - Until now',
+      description: 'I work in Saiku that offers a user friendly, web based analytics solution that lets users, quickly and easily analyse corporate data and create and share reports. The solution connects to a range of OLAP Servers including Mondrian, Microsoft Analysis Services, SAP BW and Oracle Hyperion and can be deployed rapidly and cost effectively to allow users to explore data in real time.'
+    },
+    {
+      employer: 'NUTES (UEPB)',
+      title: 'Student Researcher',
+      location: 'Brazil',
+      dates: 'July 2014 - November 2015',
+      description: 'I worked (part-time) researcher at NUTES/UEPB, where I had the opportunity to study and develop a software of electrocardiograms (ECG) and a web app for view medical records.'
+    },
+    {
+      employer: 'Microsoft',
+      title: 'Microsoft Student Partner',
+      location: 'Brazil',
+      dates: 'December 2012 - October 2015',
+      description: 'Microsoft Student Partners (MSPs) are student technology leaders, empowered to build Microsoft communities on their campus and share their deep knowledge and passion for technology with their fellow classmates.'
+    },
+    {
+      employer: 'Indra Company',
+      title: 'Systems Analyst',
+      location: 'Brazil',
+      dates: 'June 2013 - September 2014',
+      description: 'I worked in SIGSAB (Sistema de Gestão da Informação e do Conhecimento do Semiárido Brasileiro) project, for INSA (Instituto Nacional do Semiárido). The system was developed using open source projects of Business Intelligence (Pentaho BI and Saiku Analytics).'
+    },
+    {
+      employer: 'SENAI',
+      title: 'Web Developer',
+      location: 'Brazil',
+      dates: 'February 2012 - May 2013',
+      description: 'I worked with the development of web system SGM (Sistema Gerenciador de Metas) to work in the strategic management of institution and a system for students view class schedules, notes and fill institutional forms.'
+    }
+  ],
+  display: function() {
+    if (work.jobs.length > 0) {
+      $('#workExperience').append(HTMLworkStart);
+
+      for (var i = 0, len = work.jobs.length; i < len; i++) {
+        var formattedEmployer = HTMLworkEmployer.replace(DATA, work.jobs[i].employer);
+        var formattedTitle = HTMLworkTitle.replace(DATA, work.jobs[i].title);
+        var formattedDates = HTMLworkDates.replace(DATA, work.jobs[i].dates);
+        var formattedLocation = HTMLworkLocation.replace(DATA, work.jobs[i].location);
+        var formattedDescription = HTMLworkDescription.replace(DATA, work.jobs[i].description);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+        $('.work-entry').append(formattedEmployerTitle);
+        $('.work-entry').append(formattedDates);
+        $('.work-entry').append(formattedLocation);
+        $('.work-entry').append(formattedDescription);
+      }
+    }
+  }
+};
+
 bio.display();
+work.display();
